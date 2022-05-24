48797566c2d014e3588302c1260134727cd56923
Steven Zhang <steven.zhr@gmail.com>
2022-05-24 21:26:19 +0800

    test add helloworld
    haha

--- src/main/java/QueryProcessor.java
+++ src/main/java/QueryProcessor.java
@@ -33,7 +33,8 @@ public class QueryProcessor {
         qp.processQueries();
 
         // Note that you are expected to create additional classes and methods
-        // hello test commit
+        // hello test commit again
+        System.out.println("helloworld");
 
     }
 
